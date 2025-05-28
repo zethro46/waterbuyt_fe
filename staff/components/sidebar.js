7f30bb917b34b488e8783d3ac92665d7a4472794
HuyenNguyen <huyen04062003@gmail.com>
2025-05-28 13:17:17 +0700

    fix logout function of staff

--- staff/components/sidebar.js
+++ staff/components/sidebar.js
@@ -43,7 +43,7 @@ document.addEventListener("DOMContentLoaded", function() {
         logoutLink.addEventListener("click", function(event) {
             event.preventDefault(); 
             localStorage.clear(); 
-            window.location.href = "/common/pages/index.html"; 
+            window.location.href = "/admin2/homepage/index.html"; 
         });
     }
 });
